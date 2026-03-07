fc53d4f97ed98b1e121a29cda03f06f9a9ee31ef
Dribax018 <unaiangalo@gmail.com>
2026-03-07 17:15:58 +0100

    MOVIMIENTO DE PERSONAJE E IMPLEMENTACIÓN DE COLIDERS

---
+++ GameJam2026/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform player;
+
+    void Start() { }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
